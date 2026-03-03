970ca2e1eee7108f37bc5edff4f80344f010eb64
Joanne H <jlsjoanne@gmail.com>
2026-03-03 15:29:26 +0800

    feat: add Json Converter Page

--- 260303_hw_Json/JsonConverter.aspx.cs
+++ 260303_hw_Json/JsonConverter.aspx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Unicode;
 
 namespace _260303_hw_Json
 {
@@ -12,6 +15,50 @@ namespace _260303_hw_Json
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int age;
+            bool ageParse = int.TryParse(ageInput.Text, out age);
+
+            if (ageParse)
+            {
+                Student student = new Student
+                {
+                    Name = nameInput.Text,
+                    Age = age
+                };
+
+                JsonSerializerOptions _options = new JsonSerializerOptions
+                {
+                    WriteIndented = true,
+                    ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles,
+                    Encoder = JavaScriptEncoder.Create(UnicodeRanges.All)
+                };
+                string json = JsonSerializer.Serialize(student,_options);
+                convertToJson.Text = json;
+            }
+            else
+            {
+                convertToJson.Text = "年齡輸入格式不符";
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            string json = jsonInput.Text;
+
+            try
+            {
+                Student student = JsonSerializer.Deserialize<Student>(json);
+
+                convertToProperties.Text = $"姓名: {student.Name} <br /> 年齡: {student.Age}";
+            }
+            catch(Exception ex)
+            {
+                convertToProperties.Text = "錯誤: 無法反序列化JSON。 <br />" + ex.Message; 
+            }
         }
     }
 }
---
+++ 260303_hw_Json/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _260303_hw_Json
+{
+    //Define Student class with name and age
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
